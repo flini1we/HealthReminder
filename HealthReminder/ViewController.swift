e9ba37616ff8ac62cff5e08d3ec0a3979800080b
Danil Zabinskij <rexportydf@mail.ru>
2025-06-11 10:08:00 +0300

    initial commit

---
+++ HealthReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HealthReminder
+//
+//  Created by Данил Забинский on 10.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
